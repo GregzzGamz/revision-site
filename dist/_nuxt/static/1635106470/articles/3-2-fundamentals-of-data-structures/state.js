25f83f39415b43af79fed2ef75c92e1bb33f37fe
neeko <59419317+GregzzGamz@users.noreply.github.com>
2021-10-24 21:16:45 +0100

    add dist

---
+++ dist/_nuxt/static/1635106470/articles/3-2-fundamentals-of-data-structures/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635106470",layout:"default",error:a,state:{theme:{darkmode:void 0}},serverRendered:true,routePath:"\u002Farticles\u002F3-2-fundamentals-of-data-structures",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0ee33bbc"}}}}(null));
